96018ab7b09e440815c8cb5cadf93286fc0ae02f
sungjea713 <sungjea713@gmail.com>
2019-08-01 17:49:45 +0900

    Button event javascript

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+function StartClick()
+{
+    
+    var btnStart = document.getElementById("ButtonStart");
+    var btnStop = document.getElementById("ButtonStop");
+
+    btnStart.style.display = "none";
+    btnStop.style.display = "inline";
+    
+}
+
+function StopClick()
+{
+    
+    var btnStart = document.getElementById("ButtonStart");
+    var btnStop = document.getElementById("ButtonStop");
+
+    btnStart.style.display = "inline";
+    btnStop.style.display = "none";
+    
+}
